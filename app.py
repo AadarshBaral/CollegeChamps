4875c8e2ba40559952c4779ecffc50caf0af0d39
AadarshBaral <adarsmain101@gmai.com>
2021-10-11 15:18:03 +0545

    init

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import _DebugQueryTuple
 from collegechamps import app
 import os
-port = int(os.getenv('PORT'))
+port = int(os.environ.get('PORT',33507))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=port)
