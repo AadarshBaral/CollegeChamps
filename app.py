817eee2d88b8b0256f92491986526e95f4fe6908
AadarshBaral <adarsmain101@gmai.com>
2021-10-29 21:50:21 +0545

    update 1.1

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from collegechamps import app
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port = 8080)
---
+++ test.py
@@ -0,0 +1,7 @@
+def square(n):
+    k  =0
+    while True:
+        if k == n*n:
+            return k
+        k +=1
+print(square(100))
